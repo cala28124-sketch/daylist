e72df6c9d8d1ad83d69e9b72e6fed89a21d8a4ae
Walter-Rodz <124607138+Walter-Rodz@users.noreply.github.com>
2026-07-11 21:37:57 -0400

    Update GeminiTest.js

--- public/GeminiTest.js
+++ public/GeminiTest.js
@@ -5,7 +5,7 @@ async function Playlist(userPrompt)
 {
     console.log('Generating a playlist from: "${userPrompt}$"\n');
     const response = await ai.models.generativeContent({
-        model: 'gemini-2.5-flash',
+        model: 'gemini-3.5-flash',
         contents: 'Generate a list of 5 songs that match this vibe: "${userPrompt}$".',
         config:{
             systemInstruction:"You are a music expert. Output ONLY a clean, numbered list of songs with their artists. Do not include introductory text or conversational sign-offs.",
